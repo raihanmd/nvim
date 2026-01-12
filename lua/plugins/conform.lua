99b1c99cb3560ad47ebd7a47338405c55b9a0b3d
raihanmd <mraihanf471@gmail.com>
2026-01-12 18:09:42 +0700

    fix: lsp solidity

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,9 +2,14 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      format_on_save = {
+        lsp_fallback = false,
+      },
+
       formatters_by_ft = {
         solidity = { "forge_fmt" },
       },
+
       formatters = {
         forge_fmt = {
           command = "forge",
